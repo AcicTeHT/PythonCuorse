ad2de967ec4740b9e7e7dbba66cb6bdd14dfa6af
Bakhtiior Shoiev <bahtashoiev@gmail.com>
2024-02-24 21:25:05 +0100

    homeWork2

--- HomeWork_2.py
+++ HomeWork_2.py
@@ -1,1 +1,9 @@
-print('Hello world!')
+num =int(input('enter a four-digit number:'))
+last = num % 10
+first = int(num/1000)
+third = int((num - last)%100/10)
+second = int(((num-last)-(third*10) - (first * 1000))/100)
+print(first)
+print(second)
+print(third)
+print(last)
