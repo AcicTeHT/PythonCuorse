83eedec70751d2ec9f78f5debf84117cc1ab7cf4
Bakhtiior Shoiev <bahtashoiev@gmail.com>
2024-02-24 21:38:41 +0100

    homeWork_3

--- HomeWork_2.py
+++ HomeWork_2.py
@@ -13,15 +13,6 @@
 
 
 
-# homeWork_3
 
-
-num =int(input('enter a five-digit number:'))
-last = num % 10
-first = num // 10000
-second = ((num - (first * 10000)) // 1000)
-third = (((num - (first * 10000)) - second * 1000) // 100)
-fourth = ((num % 100) //10)
-print((last * 10000) + (fourth * 1000)+(third*100)+(second*10)+first)
 
 
