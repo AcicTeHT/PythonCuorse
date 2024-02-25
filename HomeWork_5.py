265e625c6192d413c2b5b72db42b407bed3e0810
Bakhtiior Shoiev <bahtashoiev@gmail.com>
2024-02-25 20:28:07 +0100

    Reworked homework

--- HomeWork_5.py
+++ HomeWork_5.py
@@ -5,11 +5,11 @@ first_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 l = []
 r = []
 my_list = []
+step_finish = len(first_list)+1
 
 
 if len(first_list) % 2 == 0:
     step = len(first_list) // 2
-    step_finish = len(first_list)+1
     r = first_list[step:step_finish]
     l = first_list[:step]
     my_list.append(l)
@@ -17,7 +17,7 @@ if len(first_list) % 2 == 0:
     print(my_list)
 elif len(first_list) % 2 == 1:
     step_2 = (round(len(first_list) / 2 + 0.5))
-    r = first_list[step_2:99]
+    r = first_list[step_2:step_finish]
     l = first_list[:step_2]
     my_list.append(l)
     my_list.append(r)
