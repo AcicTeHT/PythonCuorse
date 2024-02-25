ff837c8f8da96f318e19df45f6eb69db4d9e32e9
Bakhtiior Shoiev <bahtashoiev@gmail.com>
2024-02-25 09:22:53 +0100

    wariant

--- HomeWork_3.py
+++ HomeWork_3.py
@@ -8,3 +8,5 @@ second = ((num - (first * 10000)) // 1000)
 third = (((num - (first * 10000)) - second * 1000) // 100)
 fourth = ((num % 100) //10)
 print((last * 10000) + (fourth * 1000)+(third*100)+(second*10)+first)
+
+# first, second, third, fourth, last = last, fourth, third, second, first
