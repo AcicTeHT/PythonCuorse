480b0fc92816b001db5b117be41b9cdff16d0d09
Bakhtiior Shoiev <bahtashoiev@gmail.com>
2024-02-25 20:26:58 +0100

    Reworked homework

--- HomeWork_5.py
+++ HomeWork_5.py
@@ -1,6 +1,7 @@
 # first_list = []
 # first_list = [1, 2, 3, 4, 5, 6, 7]
-first_list = [1, 2, 3, 4, 5, 6]
+# first_list = [1, 2, 3, 4, 5, 6]
+first_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 l = []
 r = []
 my_list = []
@@ -8,7 +9,8 @@ my_list = []
 
 if len(first_list) % 2 == 0:
     step = len(first_list) // 2
-    r = first_list[step:99]
+    step_finish = len(first_list)+1
+    r = first_list[step:step_finish]
     l = first_list[:step]
     my_list.append(l)
     my_list.append(r)
