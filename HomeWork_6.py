4e49fc8e63326302a1742269818eeec19a91ecdb
Bakhtiior Shoiev <bahtashoiev@gmail.com>
2024-02-26 16:29:23 +0100

    CorrectedWork_6

--- HomeWork_6.py
+++ HomeWork_6.py
@@ -2,11 +2,20 @@ my_list = [1, 2, 3, 4,]
 # my_list = [1]
 # my_list = []
 
-new_list = []
+# new_list = []
+# if len(my_list) > 0:
+#     lastElement = my_list.pop()
+#     new_list.append(lastElement)
+#     new_list.extend(my_list)
+#     print(new_list)
+# else:
+#     print(my_list)
+
+
+
 if len(my_list) > 0:
     lastElement = my_list.pop()
-    new_list.append(lastElement)
-    new_list.extend(my_list)
-    print(new_list)
+    my_list.insert(0,lastElement)
+    print(my_list)
 else:
     print(my_list)
