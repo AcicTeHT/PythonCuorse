3bd13284631c2964c03821a76e5a108e042238ae
Bahta <bahtashoiev@gmail.com>
2024-03-04 19:41:33 +0100

    norm

--- hw_10.py
+++ hw_10.py
@@ -1,1 +1,16 @@
-print("hello world")
+import string
+import keyword
+
+name = input("your wariable name:")
+pnkt = string.punctuation
+lm_list = keyword.kwlist
+
+# print(pnkt)
+print(lm_list)
+
+# if name[0].isdigit() or name.isdigit():
+#     print("False")
+if keyword.iskeyword(name):
+    print(name)
+if name.istitle():
+    print(name)
