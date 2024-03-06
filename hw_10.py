4bb8c2408079e663e5452e3ac72e7518e79e7513
Bahta <bahtashoiev@gmail.com>
2024-03-06 12:17:45 +0100

    hw_10, not work kode

--- hw_10.py
+++ hw_10.py
@@ -5,12 +5,24 @@ name = input("your wariable name:")
 pnkt = string.punctuation
 lm_list = keyword.kwlist
 
-# print(pnkt)
-print(lm_list)
+i = 0
+while i < len(name):
+    if name[i].isspace():
+        print("False")
+    i += 1
+for i in pnkt:
+    for j in name:
+        if j == "_":
+            # print("True")
+            continue
+        elif i == j:
+            print('a')
+            # print("False")
 
-# if name[0].isdigit() or name.isdigit():
-#     print("False")
-if keyword.iskeyword(name):
-    print(name)
-if name.istitle():
-    print(name)
+if name[0].isdigit() or name.isdigit():
+    print("False_1")
+elif not name.islower() or keyword.iskeyword(name):
+    print("False_2")
+else:
+    print("True")
+
