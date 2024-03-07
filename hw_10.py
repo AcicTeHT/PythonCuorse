6104764808a04ce7d86ac8bf3422595e5eeece8e
Bahta <bahtashoiev@gmail.com>
2024-03-07 23:38:38 +0100

    hw_10

--- hw_10.py
+++ hw_10.py
@@ -1,46 +1,27 @@
 import string
 import keyword
 
-name = input("your wariable name:")
+name = input("Your variables:")
 pnkt = string.punctuation
 lm_list = keyword.kwlist
 
-
-
-# print("true_1")
-
-if len(name.split()) > 1:
-    print("False_1")
-elif name[0].isdigit() or name.isdigit():
-    print("False_3")
+if name == "_":
+    print("True")
+elif not name.islower() or keyword.iskeyword(name):
+    print("False")
 elif name.isalpha():
-    if not name.islower() or keyword.iskeyword(name):
-        print("False_2")
+    print("true")
+elif len(name.split()) > 1:
+    print("False")
+elif name[0].isdigit() or name.isdigit():
+    print("False")
+elif name.isalnum():
+    print("True")
 elif not name.isalpha():
     for i in pnkt:
         for j in name:
-            if j == "_":
-                continue
-            elif i == j:
+            if j == i:
+                if j == "_":
+                    print("True")
+                    continue
                 print("False")
-else:
-    print("True_2")
-
-
-
-
-
-# for i in pnkt:
-#     for j in name:
-#         if j == "_":
-#             continue
-#         elif i == j:
-#             print("False")
-# if len(name.split()) > 1:
-#     print("False")
-# elif name[0].isdigit() or name.isdigit():
-#     print("False_1")
-# elif not name.islower() or keyword.iskeyword(name):
-#     print("False_2")
-# else:
-#     print("True")
