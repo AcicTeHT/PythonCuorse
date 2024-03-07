dd15e39df5c7f88299e26eb78610f7e1e9b30fde
Bahta <bahtashoiev@gmail.com>
2024-03-07 12:11:22 +0100

    not work_2 hw-10

--- hw_10.py
+++ hw_10.py
@@ -6,18 +6,41 @@ pnkt = string.punctuation
 lm_list = keyword.kwlist
 
 
-for i in pnkt:
-    for j in name:
-        if j == "_":
-            continue
-        elif i == j:
-            print("False")
-if len(name.split()) > 1:
-    print("False")
-elif name[0].isdigit() or name.isdigit():
-    print("False_1")
-elif not name.islower() or keyword.iskeyword(name):
-    print("False_2")
-else:
-    print("True")
 
+# print("true_1")
+
+if len(name.split()) > 1:
+    print("False_1")
+elif name[0].isdigit() or name.isdigit():
+    print("False_3")
+elif name.isalpha():
+    if not name.islower() or keyword.iskeyword(name):
+        print("False_2")
+elif not name.isalpha():
+    for i in pnkt:
+        for j in name:
+            if j == "_":
+                continue
+            elif i == j:
+                print("False")
+else:
+    print("True_2")
+
+
+
+
+
+# for i in pnkt:
+#     for j in name:
+#         if j == "_":
+#             continue
+#         elif i == j:
+#             print("False")
+# if len(name.split()) > 1:
+#     print("False")
+# elif name[0].isdigit() or name.isdigit():
+#     print("False_1")
+# elif not name.islower() or keyword.iskeyword(name):
+#     print("False_2")
+# else:
+#     print("True")
