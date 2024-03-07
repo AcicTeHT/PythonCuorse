53ea2f847e60bf183f7d2f58bafefdeacc9cd8cf
Bahta <bahtashoiev@gmail.com>
2024-03-07 11:05:11 +0100

    reapet_11

--- hw_10.py
+++ hw_10.py
@@ -9,11 +9,9 @@ lm_list = keyword.kwlist
 for i in pnkt:
     for j in name:
         if j == "_":
-            # print("True")
             continue
         elif i == j:
-            print('a')
-            # print("False")
+            print("False")
 if len(name.split()) > 1:
     print("False")
 elif name[0].isdigit() or name.isdigit():
--- hw_11.py
+++ hw_11.py
@@ -1,10 +1,24 @@
 
 
+num1 = int(input("Please enter first number: "))
+num2 = int(input("Please enter second number:: "))
+sign = input("your sign")
 
-i = 0
-while i < 1:
-    question = input("Do you want to perform another calculation? y  " )
-    if question == "y":
+if sign == "*":
+    print(num1 * num2)
+elif sign == "+":
+    print(num1 + num2)
+elif sign == "-":
+    print(num1 - num2)
+elif sign == "/" and num2 != 0:
+    print(num1 // num2)
+else:
+    print("You can't divide by 0")
+
+question = input("Do you want to perform another calculation? y  " )
+if question == "y":
+    i = 0
+    while i < 1:
         num1 = int(input("Please enter first number: "))
         num2 = int(input("Please enter second number:: "))
         sign = input("your sign")
@@ -19,8 +33,10 @@ while i < 1:
             print(num1 // num2)
         else:
             print("You can't divide by 0")
-    else:
-        i = 1
+
+        question = input("Do you want to perform another calculation? y  ")
+        if not question == "y":
+            i += 1
 
 
 
