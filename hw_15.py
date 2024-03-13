83e5b5a268225ca285824f4aaf211c2685339966
Bahta <bahtashoiev@gmail.com>
2024-03-13 17:32:20 +0100

    hw_15

--- hw_15.py
+++ hw_15.py
@@ -3,24 +3,28 @@
 num = int(input("Enter your number: "))
 my_str = ""
 
-if num >= 0 or num < 8640000:
+if num >= 0 and num < 8640000:
     name = ""
     seconds = num%60
     minutes = (num // 60) % 60
     hours = (num//60//60)%24
     days = (num//60//60//24)
-    if days == 1 or (days > 10 and (days % 10) == 1):
+    if days == 1 or (days > 21 and (days % 10) == 1):
         name = "день"
-    elif days >= 2 or days < 5 :
+    elif ((days >= 2) and (days < 5)) or (days > 20 and ((days % 10 >= 2) and (days % 10 < 5))):
         name = "дні"
-    elif (days >= 5 or (days % 10) >= 5) or (days <=  or days == 0) or (days == 0 or (days % 10) == 1):
+    elif ((days >= 5 and days <= 20) or (days % 10) >= 5) or days == 0:
         name = "днів"
-    # elif (days % 10) >= 5 and days <= 20 or days == 0:
-    #     name = "днів"
 
-    my_str = "{d}:{n}, {h}:{m}:{s}".format(n=name, d=days, h=hours, m=minutes, s=seconds)
+    if seconds < 10:
+        seconds = str(seconds).zfill(2)
+    if minutes < 10:
+        minutes = str(minutes).zfill(2)
+    if hours < 10:
+        hours = str(hours).zfill(2)
+
+    my_str = "{d} {n}, {h}:{m}:{s}".format(n=name, d=days, h=hours, m=minutes, s=seconds)
     print(my_str)
-    print(days % 10)
 else:
     print('Incorrect number')
 
