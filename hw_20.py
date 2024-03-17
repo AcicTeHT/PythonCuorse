91fc93e130c1a485ec6656bfba987508d65f0269
Bahta <bahtashoiev@gmail.com>
2024-03-17 12:39:34 +0100

    hw_20

---
+++ hw_20.py
@@ -0,0 +1,18 @@
+dict_example = {"a": 1, "b": 2}
+
+def find_unique_value(some_list):
+    di ={}
+    for i in some_list:
+        if i not in di:
+            di[i] = 1
+        else:
+            di[i] += 1
+    for i,n in di.items():
+        if n == 1:
+            return i
+
+assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
+assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
+assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
+print("ОК")
+
