9dbed31b7ff7e011042c0d766a24926b7b85a9ad
Bahta <bahtashoiev@gmail.com>
2024-03-18 16:15:52 +0100

    hw_17_2

--- hw_17.py
+++ hw_17.py
@@ -1,13 +1,17 @@
 
-sentence = input("Your sentence: ")
-
 def correct_sentence(text):
-    fin = text[-1]
-    if fin != ".":
-        # text = "{text.capitalize()}.".format(text=text)  #alternative
-        text = text.capitalize() + "."
-        print(text)
+    text = text[0].upper() + text[1:]
+    if text[-1] != ".":
+        text = text + "."
+        return text
     else:
-        print(text.capitalize())
+        return text
 
-correct_sentence(sentence)
+
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+print('ОК')
+
