2d25d85bded3c88c5b3482c08f0701a685899a14
Bahta <bahtashoiev@gmail.com>
2024-03-18 15:46:21 +0100

    hw_16_2

--- hw_16.py
+++ hw_16.py
@@ -1,12 +1,10 @@
-name = input("Your name: ")
-age = int(input("Your age: "))
 
-name = name.capitalize()
+def say_hi(name, age):
 
-def sey_hi():
-    if age > 0:
-        res = "Hi. My name is {name} and I'm {age} years old ".format(name=name,age=age)
-        print(res)
-    else:
-        print("Age is not positive")
-sey_hi()
+    res = "Hi. My name is {name} and I'm {age} years old".format(name=name,age=str(age))
+    return res
+
+
+assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
+assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
+print('ОК')
