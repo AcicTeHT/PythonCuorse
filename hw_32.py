47ef3530b399cb5b646c6d99ee04a63edc4f97ad
Bahta <bahtashoiev@gmail.com>
2024-04-07 17:09:04 +0200

    hw_32

--- hw_32.py
+++ hw_32.py
@@ -4,22 +4,28 @@ class Fraction:
         self.b = b
 
     def __mul__(self, other):
-        return Fraction(self.a * other.a, self.b * other.b)
+        if isinstance(other, Fraction):
+            return Fraction(self.a * other.a, self.b * other.b)
 
     def __add__(self, other):
-        return Fraction(self.a * other.b + other.a * self.b, self.b * other.b)
+        if isinstance(other, Fraction):
+            return Fraction(self.a * other.b + other.a * self.b, self.b * other.b)
 
     def __sub__(self, other):
-        return Fraction(self.a * other.b - other.a * self.b, self.b * other.b)
+        if isinstance(other, Fraction):
+            return Fraction(self.a * other.b - other.a * self.b, self.b * other.b)
 
     def __eq__(self, other):
-        return self.a * other.b == other.a * self.b
+        if isinstance(other, Fraction):
+            return self.a * other.b == other.a * self.b
 
     def __gt__(self, other):
-        return self.a * other.b > other.a * self.b
+        if isinstance(other, Fraction):
+            return self.a * other.b > other.a * self.b
 
     def __lt__(self, other):
-        return self.a * other.b < other.a * self.b
+        if isinstance(other, Fraction):
+            return self.a * other.b < other.a * self.b
 
     def __str__(self):
         return f"Fraction: {self.a}, {self.b}"
